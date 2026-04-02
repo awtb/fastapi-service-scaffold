bf679931b376cd1fa61c878d9314b184cbf54776
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-04-02 18:07:05 +0500

    refactor(users): export users infra adapters

--- template/{{package_name}}/features/users/infra/__init__.py
+++ template/{{package_name}}/features/users/infra/__init__.py
@@ -1,11 +1,15 @@
+from .jwt import JwtAccessTokenService
+from .passwords import Pbkdf2PasswordService
 from .repository import build_users_repository
-from .mappers import create_user_record
 from .mappers import to_user_model
+from .mappers import to_user_credentials
 from .repository import SqlAlchemyUsersRepository
 
 __all__ = [
+    "JwtAccessTokenService",
+    "Pbkdf2PasswordService",
     "SqlAlchemyUsersRepository",
     "build_users_repository",
-    "create_user_record",
     "to_user_model",
+    "to_user_credentials",
 ]
