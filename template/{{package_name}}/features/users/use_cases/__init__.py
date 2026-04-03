e5d39e0a83a224629191f36be110aa743af75a9b
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-04-03 16:26:29 +0500

    refactor(users): export get current user use case

--- template/{{package_name}}/features/users/use_cases/__init__.py
+++ template/{{package_name}}/features/users/use_cases/__init__.py
@@ -1,5 +1,11 @@
 from .authenticate_user import AuthenticateUserUseCase
+from .get_current_user import GetCurrentUserUseCase
 from .get_user import GetUserUseCase
 from .list_users import ListUsersUseCase
 
-__all__ = ["AuthenticateUserUseCase", "GetUserUseCase", "ListUsersUseCase"]
+__all__ = [
+    "AuthenticateUserUseCase",
+    "GetCurrentUserUseCase",
+    "GetUserUseCase",
+    "ListUsersUseCase",
+]
