5e3cb4a0aec95392b807094d299ceac7be00b6a9
Ilyas Qalandarzoda <ilyas.qalandarzoda@icloud.com>
2026-04-03 11:29:27 +0500

    refactor(users): trim users infra exports

--- template/{{package_name}}/features/users/infra/__init__.py
+++ template/{{package_name}}/features/users/infra/__init__.py
@@ -1,15 +1,11 @@
 from .jwt import JwtAccessTokenService
 from .passwords import Pbkdf2PasswordService
-from .repository import build_users_repository
-from .mappers import to_user_model
-from .mappers import to_user_credentials
 from .repository import SqlAlchemyUsersRepository
+from .repository import build_users_repository
 
 __all__ = [
     "JwtAccessTokenService",
     "Pbkdf2PasswordService",
     "SqlAlchemyUsersRepository",
     "build_users_repository",
-    "to_user_model",
-    "to_user_credentials",
 ]
